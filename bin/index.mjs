bb95a9605529a494caa70321540f89f08bfd6db6
Takowski <93830775+Takowski@users.noreply.github.com>
2024-01-03 15:35:02 +0100

    done

--- bin/index.mjs
+++ bin/index.mjs
@@ -1,6 +1,13 @@
 import axios from "axios";
 import countryList from "country-list";
 
-for(let i = 0; i < process.argv.length; ++i) {
-	console.log(`index ${i} argument -> ${process.argv[i]}`);
+const getCountryInfo = async (countryName) => {
+    const countryCode = countryList.getCode(countryName);
+    const year = new Date().getFullYear(); 
+    console.log(countryCode);  
+    const url = `https://date.nager.at/api/v3/publicholidays/${year}/${countryCode}`;
+    const response = await axios.get(url);
+    return response.data;
 }
+const countryName = process.argv[2] || "Belgium";
+console.log(await getCountryInfo(countryName));
